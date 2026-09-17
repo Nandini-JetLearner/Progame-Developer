ef258b4403ce98503d5223796c18889cb4e2d854
Nandini M. <deepanjan.mukherjee@gmail.com>
2026-09-17 18:49:04 -0400

    Add files via upload

--- HOMEWORK/FruitSpawn/fruit.py
+++ HOMEWORK/FruitSpawn/fruit.py
@@ -14,12 +14,15 @@ player_y=300
 speed=15
 x=300
 y=0
+x2=400
+y2=0
 fruits=0
 increase=5
 
 player=pygame.image.load("basket.png")
 background=pygame.image.load("corn.jpg")
 apple=pygame.image.load("fruit.png")
+banana=pygame.image.load("fruit2.png")
 
 font=pygame.font.SysFont("Times New Roman", 28)
 
@@ -29,22 +32,30 @@ while running:
     screen.blit(background,(0,0))
     screen.blit(player,(player_x,player_y))
     screen.blit(apple,(x,y))
+    screen.blit(banana,(x2,y2))
     Text = font.render("Fruits: ",False,(0,0,0))
     Text2 = font.render(str(fruits),False,(0,0,0))
     screen.blit(Text,(100,50)) 
-    screen.blit(Text2,(170,50))
+    screen.blit(Text2,(180,50))
     pygame.display.flip()
 
     for event in pygame.event.get():
             if event.type==pygame.QUIT:
                 running=False
     if y >550:
-        increase+=1
+        increase+=0.5
         x=random.randint(20,520)
         y=0
 
+    if y2 >550:
+            increase+=0.5
+            x2=random.randint(20,520)
+            y2=0
+              
+
 
     apple_rect = pygame.Rect(x, y, apple.get_width(), apple.get_height())
+    banana_rect = pygame.Rect(x2, y2, banana.get_width(), banana.get_height())
     player_rect = pygame.Rect(player_x, player_y, player.get_width(), player.get_height())
 
     
@@ -60,15 +71,25 @@ while running:
         player_x+=speed
 
     y+=increase
+    y2+=increase
 
     if player_rect.colliderect(apple_rect):
         fruits+=1
-        print("Caught a fruit!")
+        print("Caught an apple!")
         x=random.randint(20,520)
         y=0
 
-    if fruits==15:
+
+    if player_rect.colliderect(banana_rect):
+        fruits+=2
+        print("Caught a banana!")
+        x2=random.randint(20,520)
+        y2=0
+
+
+    if fruits==30 or fruits>30:
         print("Game Over!")
+        print(str(fruits) + " Fruits Collected!")
         running=False
 
 
